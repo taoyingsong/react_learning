937389e931420f2782a5ea9908e0f16105cbfc77
taoyingsong <yingsongtys@163.com>
2016-03-29 11:01:43 +0800

    new demo

---
+++ react-webpack/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry:[
+    // './app/main.js'
+  ],
+  output: {
+    path: __dirname + '/assets/',
+    publicPath: "/assets/",
+    filename: 'bundle.js'
+  },
+  module: {
+	  loaders: [
+	    { test: /\.jsx?$/, loaders: ['jsx?harmony']}
+	  ]
+	}
+};
